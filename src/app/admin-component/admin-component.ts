03ab92765b4cbe81be0e68c7809fd14c6753cd3a
vathsav-cogni <Bukka.SaiSriVathsav@cognizant.com>
2025-09-25 16:36:54 +0530

    admin component

--- src/app/admin-component/admin-component.ts
+++ src/app/admin-component/admin-component.ts
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './admin-component.css'
 })
 export class AdminComponent {
+isSidenavOpen = true;
+  isDisplay = "Dashboard";
+  isId = "";
 
+  parkingEvent() {
+    this.isDisplay = "Find Parking";
+  }
+  bookEvent(data : string){
+    this.isDisplay = "Vehicle logs";
+    this.isId = data;
+
+  }
+  reserveEvent(data : string){
+    this.isDisplay = "My Reservation";
+    this.isId = data;
+  }
+
+  onToggle(isOpen: boolean) {
+    this.isSidenavOpen = isOpen;
+  }
+
+  onDisplayClicked(item : string){
+    this.isDisplay = item;
+  }
 }
