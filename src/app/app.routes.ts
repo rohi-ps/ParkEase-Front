4a239be4196962191798fe706ce8597065efe0b6
rohi-ps <rohit.sudarsanan@cognizant.com>
2025-09-26 11:56:49 +0530

    import fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { UserComponent } from './user-component/user-component';
 import { AdminComponent } from './admin-component/admin-component';
-import { AuthGuard } from './Services/auth.guard';
 
 
 export const routes: Routes = [
