e4ed2a7ece339a447b83345de126a21be9981232
vathsav-cogni <Bukka.SaiSriVathsav@cognizant.com>
2025-09-30 10:16:48 +0530

    dash

--- src/app/homepage/login/login.ts
+++ src/app/homepage/login/login.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../Services/auth.service';
+import { RouterLink } from "@angular/router";
 declare var bootstrap: any;
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
