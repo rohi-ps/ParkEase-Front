bf926dacf5b3bc201c55b1fd12f58e03ec5899d3
Durgaprasadvuddangi <SaiVenkataDurgaPrasad.Vuddangi@cognizant.com>
2025-11-11 14:53:39 +0530

    changes

--- src/app/Services/auth.interceptor.ts
+++ src/app/Services/auth.interceptor.ts
@@ -10,10 +10,10 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 // Corrected import path for AuthService, assuming 'Services' with a capital S
 import { AuthService } from '../Services/auth.service';
-
+import { Router } from '@angular/router';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router:Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Get the auth token from the service
@@ -29,8 +29,8 @@ export class AuthInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
-          this.authService.logout();
-          // this.router.navigate(['/login']);
+          // this.authService.logoutfunction();
+          this.router.navigate(['/login']);
         }
         return throwError(() => error);
       })
